2aca8f27f1bf73efae53dc463ffa012082e46d22
Maniteja Pratha <61357713+Manitej66@users.noreply.github.com>
2020-07-04 12:40:16 +0530

    initial

---
+++ precache-manifest.8f00034cfcae1e98c30491898a7ca10d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5e2541e77b01ec0df81bdc49c51677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4daa69a3c103a519caa4",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "95fc0a90323d22255835",
+    "url": "/static/css/main.f5e31530.chunk.css"
+  },
+  {
+    "revision": "4daa69a3c103a519caa4",
+    "url": "/static/js/2.0ada606d.chunk.js"
+  },
+  {
+    "revision": "1eaeabb454820cc573c204925fac00cb",
+    "url": "/static/js/2.0ada606d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fc0a90323d22255835",
+    "url": "/static/js/main.7a2b91a7.chunk.js"
+  },
+  {
+    "revision": "7f4c4448ba1e31bc3bd2",
+    "url": "/static/js/runtime-main.379fbcf1.js"
+  }
+]);
